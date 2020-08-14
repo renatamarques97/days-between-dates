aa8c92944e2618904c5034dbb944519602ab857b
Renata Marques <renata.nrmarques@gmail.com>
2020-08-14 14:25:02 -0300

    add bootstrap custom

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 
+import './bootstrap_custom.js'
+import '../stylesheets/application'
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/themes/airbnb.css';
 
