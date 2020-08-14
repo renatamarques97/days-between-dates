5bebe77ad4d02549de3a82664a2c1aa4fb76882e
Renata Marques <renata.nrmarques@gmail.com>
2020-08-13 23:56:43 -0300

    add range plugin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,20 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 
-import flatpickr from 'flatpickr'
-import 'flatpickr/dist/flatpickr.min.css'
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/themes/airbnb.css';
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-flatpickr(".datepicker", {
+// flatpickr(".datepicker", {
+//   altInput: true,
+//   enableTime: true
+// });
+flatpickr("#start_date", {
   altInput: true,
-  enableTime: true
+  enableTime: true,
+  "plugins": [new rangePlugin({ input: "#end_date" })]
 });
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
